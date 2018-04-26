df98d453ccddf9d0d45f3636fc5fc32ea5470d6b
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2018-04-26 22:42:23 +0900

    Fix azure-scp-from and azure-scp-to

--- azure-alias.sh
+++ azure-alias.sh
@@ -7,8 +7,13 @@ alias azure-ssh='ssh -i ${privateKeyPath} -o "StrictHostKeyChecking no" ubuntu@$
 
 alias azure-ip='export instanceId=`az vm list-ip-addresses --name ${vmName} | jq .[].virtualMachine.network.publicIpAddresses[0].ipAddress | tr -d \"`;echo $instanceId'
 
-alias azure-scp-from='scp -i ${privateKeyPath} ubuntu@${instanceId}:$1 $2'
-alias azure-scp-to='scp -i ${privateKeyPath} $1 ubuntu@${instanceId}:$2'
+function azure-scp-from() {
+  scp -i ${privateKeyPath} ubuntu@${instanceId}:"$1" $2
+}
+
+function azure-scp-to() {
+    command scp -i ${privateKeyPath} $1 ubuntu@${instanceId}:"$2"
+}
 
 #alias azure-start-jupyter='ssh -i ${privateKeyPath} ubuntu@${instanceId} "export PATH=~/anaconda3/bin:$PATH;source activate fastai;cd ${jupyterRootDir};jupyter notebook"'
 alias azure-start-jupyter='ssh -i ${privateKeyPath} -o "StrictHostKeyChecking no" ubuntu@${instanceId} "export PATH=~/anaconda3/bin:$PATH;source activate fastai;cd ${jupyterRootDir};jupyter notebook"'
