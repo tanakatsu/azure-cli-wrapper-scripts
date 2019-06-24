08a29d3dac3cc4f56a20799592ca05269651e0c1
Tsuyoshi Tanaka <tanakatsu1080@gmail.com>
2019-06-24 22:50:52 +0900

    Support port forwarding

--- azure-alias.sh
+++ azure-alias.sh
@@ -6,6 +6,7 @@ alias azure-start='az vm start --resource-group ${resourceGroup} --name ${vmName
 alias azure-stop='az vm stop --resource-group ${resourceGroup} --name ${vmName} && az vm deallocate --resource-group ${resourceGroup} --name ${vmName}'
 #alias azure-ssh='ssh -i ${privateKeyPath} ubuntu@${instanceId}'
 alias azure-ssh='ssh -i ${privateKeyPath} -o "StrictHostKeyChecking no" ubuntu@${instanceId}'
+alias azure-ssh-portforward='ssh -i ${privateKeyPath} -o "StrictHostKeyChecking no" -L ${portForwardLocalPort}:localhost:8888 ubuntu@${instanceId}'
 
 alias azure-ip='export instanceId=`az vm list-ip-addresses --name ${vmName} | jq .[].virtualMachine.network.publicIpAddresses[0].ipAddress | tr -d \"`;echo $instanceId'
 
--- azure-env.sh
+++ azure-env.sh
@@ -3,4 +3,5 @@
 export vmName=YourVmName
 export resourceGroup=YourResourceName
 export privateKeyPath=PathToPrivateKey # ~/.ssh/your_azure_private_key
+export portForwardLocalPort=8888
 export jupyterRootDir=. # relative path from home directory
